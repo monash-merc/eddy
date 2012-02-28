2391daf7794b7de542a612dc355738ca24b0aa00
dr.jr.cleverly@gmail.com <dr.jr.cleverly@gmail.com@c897f2a8-f5f5-8552-8c0d-32e2475006ae>
2012-02-28 05:17:55 +0000

    qcts.ComputeDailySums: leap-year support added

--- qccode/trunk/scripts/qcts.py
+++ qccode/trunk/scripts/qcts.py
@@ -2412,7 +2412,10 @@ def write_sums(cf,ds,ThisOne,xlCol,xlSheet,DoSum='False',DoMinMax='False',DoMean
         if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
             dRan = 31
         if month == 2:
-            dRan = 28
+            if ds.series['Year']['Data'][0] % 4 == 0:
+                dRan = 29
+            else:
+                dRan = 28
         if month == 4 or month == 6 or month == 9 or month == 11:
             dRan = 30
             
