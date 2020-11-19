dcb019f8ec9c1b91e057d3d958dcaca7437f9f18
simonyuau <xm.yuau@gmail.com>
2020-11-19 14:32:16 +1100

    Create XssInterceptor.java
    added xss interceptor to fix xss security issue.

---
+++ src/java/au/edu/monash/merc/capture/struts2/intercepter/XssInterceptor.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2011-2013, Monash e-Research Centre
+ * (Monash University, Australia)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 	* Redistributions of source code must retain the above copyright
+ * 	  notice, this list of conditions and the following disclaimer.
+ * 	* Redistributions in binary form must reproduce the above copyright
+ * 	  notice, this list of conditions and the following disclaimer in the
+ * 	  documentation and/or other materials provided with the distribution.
+ * 	* Neither the name of the Monash University nor the names of its
+ * 	  contributors may be used to endorse or promote products derived from
+ * 	  this software without specific prior written permission.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package au.edu.monash.merc.capture.struts2.intercepter;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import com.opensymphony.xwork2.util.ValueStack;
+import org.apache.log4j.Logger;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * XssInterceptor class extends AbstractInterceptor which provides the Interceptor of request for avoid Xss attack
+ * <p>
+ * https://www.javacodegeeks.com/2012/07/anti-cross-site-scripting-xss-filter.html
+ *
+ * @author Simon Yu
+ * <p/>
+ * Email: xiaoming.yu@monash.edu
+ * @version 1.0
+ * @since 1.0
+ * <p/>
+ * Date: 21/12/11 1:27 PM
+ */
+public class XssInterceptor extends AbstractInterceptor {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        ActionContext actionContext = invocation.getInvocationContext();
+        Map<String, Object> parameters = actionContext.getParameters();
+        if (parameters != null) {
+            ValueStack stack = actionContext.getValueStack();
+            for (Map.Entry<String, Object> map : parameters.entrySet()) {
+                String value = ((String[]) (map.getValue()))[0];
+                String strip_xss_value = stripXSS(value);
+                if (logger.isDebugEnabled()) {
+                    logger.debug("parameter value: " + value);
+                    logger.debug("strip_xss_value: " + strip_xss_value);
+                }
+                stack.setValue(map.getKey(), strip_xss_value);
+            }
+        }
+        return invocation.invoke();
+    }
+
+    private String stripXSS(String value) {
+        if (value != null) {
+            // NOTE: It's highly recommended to use the ESAPI library and uncomment the following line to
+            // avoid encoded attacks.
+            // value = ESAPI.encoder().canonicalize(value);
+            //value = StringEscapeUtils.escapeHtml4(value);
+            // Avoid null characters
+            value = value.replaceAll("", "");
+
+            // Avoid anything between script tags
+            Pattern scriptPattern = Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            // Avoid anything in a src='...' type of expression
+            scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            // Remove any lonesome </script> tag
+            scriptPattern = Pattern.compile("</script>", Pattern.CASE_INSENSITIVE);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            // Remove any lonesome <script ...> tag
+            scriptPattern = Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            // Avoid eval(...) expressions
+            scriptPattern = Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            // Avoid expression(...) expressions
+            scriptPattern = Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            // Avoid javascript:... expressions
+            scriptPattern = Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            // Avoid vbscript:... expressions
+            scriptPattern = Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE);
+            value = scriptPattern.matcher(value).replaceAll("");
+
+            // Avoid onload= expressions
+            scriptPattern = Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+            value = scriptPattern.matcher(value).replaceAll("");
+        }
+        return value;
+    }
+
+}
